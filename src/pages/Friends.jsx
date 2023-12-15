80670475f7f884858cc6d6704e4a89f7242ee74d
Victor98k <Victorgustav98@gmail.com>
2023-12-15 15:00:46 +0100

    Added Footer to all pages

--- src/pages/Friends.jsx
+++ src/pages/Friends.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Nav from "../components/Nav";
+import Footer from "../components/Footer";
 
 const Friends = () => {
   return (
     <>
       <Nav />
       <h1>Friends</h1>
+      <Footer />
     </>
   );
 };
