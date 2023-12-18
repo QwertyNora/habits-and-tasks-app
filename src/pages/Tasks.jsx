5eead92303f0cac896ab0e6b7f3e67ee9a8f4d2f
Victor98k <Victorgustav98@gmail.com>
2023-12-18 15:51:29 +0100

    added start fetures to Tasks and Newtasks

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+// Tasks.js
+import React, { useState } from "react";
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
+import NewTask from "./NewTask";
 
 const Tasks = () => {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (newTask) => {
+    setTasks([...tasks, newTask]);
+  };
+
   return (
     <>
       <Nav />
+
       <h1>Tasks</h1>
+      <ul>
+        {tasks.map((task) => (
+          <li>{task.title}</li>
+        ))}
+      </ul>
+      <NewTask addTask={addTask} />
       <Footer />
     </>
   );
