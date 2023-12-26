bdb8798bb1470444f778adc66d823e4e507b46b9
Victor98k <Victorgustav98@gmail.com>
2023-12-26 10:44:26 +0100

    styled nav and made it responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,38 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import Styles from "../styles/Nav.module.css"
+import Styles from "../styles/Nav.module.css";
+import { IoCloseOutline } from "react-icons/io5";
+import { IoMenuOutline } from "react-icons/io5";
 
 const Nav = () => {
+  const [mobile, setMobile] = useState(false);
 
-  const showAlert = () => {
-    alert("We are still workin on this feature");
+  const toggleMobileMenu = () => {
+    setMobile(!mobile);
   };
 
   return (
     <>
       <nav className={Styles.navbar}>
-      <div className={Styles.appName}>
-        <h1>FragmentFriends</h1>
-      </div>
-      <ul className={Styles.linkList}>
-        <li className={Styles.linkListItem}>
-          <Link className={Styles.link} to="/">Home</Link>
-        </li>
-        <li className={Styles.linkListItem}>
-          <Link className={Styles.link} to="/habits">Habits</Link>
-        </li>
-        <li className={Styles.linkListItem}>
-          <Link className={Styles.link} to="/tasks">Tasks</Link>
-        </li>
-        <li className={Styles.linkListItem}>
-          <Link className={Styles.link} to="/friends">Friends</Link>
-        </li>
-      </ul>
-      <div>
-        <button onClick={showAlert} className={Styles.button}>Sign In</button>
-        <button onClick={showAlert} className={Styles.button}>Create Account</button>
-      </div>
-    </nav>
+        <div className={Styles.container}>
+          <h1 className={Styles.logo}>FragmentFriends</h1>
+          <ul
+            className={
+              mobile ? Styles["nav-links-mobile"] : Styles["nav-links"]
+            }
+          >
+            <li>
+              <Link className={Styles.link} to="/">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link className={Styles.link} to="/habits">
+                Habits
+              </Link>
+            </li>
+            <li>
+              <Link className={Styles.link} to="/tasks">
+                Tasks
+              </Link>
+            </li>
+            <li>
+              <Link className={Styles.link} to="/friends">
+                Friends
+              </Link>
+            </li>
+          </ul>
+          <button
+            id="hamburger-menu"
+            className={Styles.mobileMenuIcon}
+            onClick={toggleMobileMenu}
+          >
+            {mobile ? (
+              <IoCloseOutline size={45} color="white" />
+            ) : (
+              <IoMenuOutline size={45} color="white" />
+            )}
+          </button>
+          <button className={Styles.loginButton}>Log in</button>
+          <button className={Styles.signupButton}>Sign up</button>
+        </div>
+      </nav>
     </>
   );
 };
