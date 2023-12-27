78d3ca5e40c7ae92ec3cc651d0d3c84c07e5c8d8
Victor98k <Victorgustav98@gmail.com>
2023-12-27 09:42:47 +0100

    Styling for new task

--- src/pages/NewTask.jsx
+++ src/pages/NewTask.jsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 import Nav from "../components/Nav";
 import Styles from "../styles/NewTasks.module.css";
 import Footer from "../components/Footer";
+import bgimage from "../pics/undraw_accept_tasks_re_09mv.svg";
+import undraw from "../pics/undraw_next_tasks_re_5eyy.svg";
 
 const NewTask = () => {
   const [tasks, setTasks] = useState(() => {
@@ -54,69 +56,79 @@ const NewTask = () => {
   return (
     <>
       <Nav />
-      <h1 className={Styles.newTask}>New Task</h1>
-      <p className={Styles.newTask}>Add your new Tasks here!</p>
-      <h2 className={Styles.newTask}>Create New Task</h2>
-      <div className={Styles.testTwo}>
-        <form onSubmit={handleCreateTask}>
-          <div className={Styles.labelContainer}>
-            <label className={Styles.labelNewTask}>
-              Title:
-              <input
-                className={Styles.inputTitle}
-                type="text"
-                placeholder="Add Title"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-              />
-            </label>
-            <br />
-            <label className={Styles.labelNewTask}>
-              Task Type:
-              <select
-                value={taskType}
-                onChange={(e) => setTaskType(e.target.value)}
-              >
-                <option value="">Select Type</option>
-                <option value="work related">Work related</option>
-                <option value="for fun">for fun</option>
-                <option value="Chores">Chores</option>
-              </select>
-            </label>
-            <br />
-            <label className={Styles.input}>
-              Due Date:
-              <input
-                type="date"
-                value={dueDate}
-                onChange={(e) => setDueDate(e.target.value)}
-              />
-            </label>
-          </div>
-          <br />
-          <div className={Styles.btnContainer}>
-            <button type="button" onClick={handleSuggestRandomActivity}>
-              Get Random Activity
-            </button>
+      <div className={Styles.tasksWrapper}>
+        <div className={Styles.tasksText}>
+          <h3>Add new Task</h3>
 
-            <button type="submit">Create Task</button>
-            <Link to={{ pathname: "/tasks" }}>
-              <button>See all tasks</button>
-            </Link>
+          <h1 className={Styles.newTaskH1}>New task</h1>
+          <p>On this page you can add a new Task!</p>
+          <div className={Styles.bgImage}>
+            <img className={Styles.svgImage} src={bgimage} alt="" />
           </div>
-        </form>
-        <div className={Styles.newTaskContainer}>
-          {latestTask && (
-            <div className={Styles.task}>
-              <h2>New Added Task: </h2>
+        </div>
+        <div className={Styles.testTwo}>
+          <form onSubmit={handleCreateTask}>
+            <div className={Styles.labelContainer}>
+              <label className={Styles.labelNewTask}>
+                Title:
+                <input
+                  className={Styles.inputTitle}
+                  type="text"
+                  placeholder="Add Title"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+              </label>
+
               <br />
-              <p>Title: {latestTask.title}</p>
+              <label className={Styles.labelNewTask}>
+                Task Type:
+                <select
+                  value={taskType}
+                  onChange={(e) => setTaskType(e.target.value)}
+                >
+                  <option value="">Select Type</option>
+                  <option value="work related">Work related</option>
+                  <option value="for fun">for fun</option>
+                  <option value="Chores">Chores</option>
+                </select>
+              </label>
               <br />
-              <p>Task Type: {latestTask.taskType}</p>
-              <br />
-              <p>Due Date: {latestTask.dueDate}</p>
+
+              <label className={Styles.input}>
+                Due Date:
+                <input
+                  type="date"
+                  value={dueDate}
+                  onChange={(e) => setDueDate(e.target.value)}
+                />
+              </label>
             </div>
-          )}
+            <br />
+            <div className={Styles.btnContainer}>
+              <button type="button" onClick={handleSuggestRandomActivity}>
+                Get Random Activity
+              </button>
+
+              <button type="submit">Create Task</button>
+              <Link to={{ pathname: "/tasks" }}>
+                <button>See all tasks</button>
+              </Link>
+            </div>
+          </form>
+          <div className={Styles.newTaskContainer}>
+            {latestTask && (
+              <div className={Styles.task}>
+                <h2>New Added Task: </h2>
+                <br />
+                <p>Title: {latestTask.title}</p>
+                <br />
+                <p>Task Type: {latestTask.taskType}</p>
+                <br />
+                <p>Due Date: {latestTask.dueDate}</p>
+              </div>
+            )}
+          </div>
         </div>
       </div>
       <Footer />
